f3682e157ba8f2f165636f5222889d1e343cba6c
qoridodianto <qdodianto26@gmail.com>
2021-08-11 10:50:50 +0700

    Syifa Inna RZ

---
+++ latihan 1 tgl 11 agustus.R
@@ -0,0 +1,19 @@
+# Fungsi dasar
+teks1 <- "aku adalah anak gembala"
+teks2 <- "selalu riang serta gembira"
+
+# Fungsi if dan else
+Tim_syifa = 50
+Tim_qori = 30
+
+if (Tim_syifa > Tim_qori){
+  print("Tim syifa menang")
+}
+
+Tim_syifa = 40
+Tim_qori = 25
+
+if (Tim_syifa > Tim_qori) {
+  print("Tim syifa menang")
+} else{ print ("Tim qori menang")
+}
